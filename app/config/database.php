7f747ac33c66a3f90547090825b8359e3b70c9c8
Rizqy Hidayat <rizqy22@gmail.com>
2012-07-25 14:35:12 +0700

    clearing database information

--- app/config/database.php
+++ app/config/database.php
@@ -1,13 +1,12 @@
 <?php
-
 return array(
 	'default' => array(
 		'driver' => 'mysql',
 		'host' => 'localhost',
 		'port' => 3306,
-		'user' => 'root',
-		'password' => 'roottoor',
-		'database' => 'kopizine',
+		'user' => '',
+		'password' => '',
+		'database' => '',
 		'tablePrefix' => '',
 		'charset' => 'utf8',
 		'collate' => 'utf8_general_ci',
