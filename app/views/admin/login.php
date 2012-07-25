ff0b1fe1c227bdc18bb1b8851da8baf77c9e2b14
Rizqy Hidayat <rizqy22@gmail.com>
2012-07-25 15:11:50 +0700

    better login page layout

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,14 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-<h3>Login</h3>
-<form action="" method="post">
-    <label for="username">Username:</label> <input type="text" name="username" id="username"> (admin)<br />
-    <label for="password">Password:</label> <input type="password" name="password" id="password"> (admin)<br />
-    <input type="submit" name="submit" id="submit" value="Login">
-</form>
-</body>
-</html>
+<?php include 'header.php'; ?>
+
+	<div id="header">
+	<header class="container">
+		<h1 id="site-title" class="columns four"><a href="#">Coffecup</a></h1>
+	</header>
+	</div>
+	
+	<div id="main" class="container">
+		<section id="page-header" class="columns sixteen row">
+			<h2>Login</h2>
+		</section>
+		
+		<section id="content" class="columns sixteen">
+			<form action="" method="post" id="login-form">
+				<div class="control-group">
+					<label class="control-label" for="username">Username:</label>
+					<div class="controls">
+						<input type="text" class="input-xlarge" id="username" name="username">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="password">Password:</label>
+					<div class="controls">
+						<input type="password" class="input-xlarge" id="password" name="password">
+					</div>
+				</div>
+				<input type="submit" name="submit" id="submit" value="Login">
+			</form>
+		</section>
+	</div><!-- end #main -->
+
+<?php include 'footer.php'; ?>
