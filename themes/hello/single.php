4da100755423069aee7886fc710a448fe64ada04
Rizqy Hidayat <rizqy22@gmail.com>
2012-08-02 15:07:11 +0700

    moving single view to theme folder

---
+++ themes/hello/single.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+<style type="text/css"><?php echo $post->css; ?></style>
+    
+    <header class="page-header">
+<h1><?php echo $post->title; ?></h1>
+</header>
+
+<?php echo $post->html; ?>
+
+<?php include 'footer.php'; ?>
